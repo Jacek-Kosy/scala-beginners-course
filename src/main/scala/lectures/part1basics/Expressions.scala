4a57dfc909888e3b891c94fa3ce197c845a62e72
Jacek <jacek.kosy@gmail.com>
2022-03-30 21:27:18 +0200

    Initial commit

---
+++ src/main/scala/lectures/part1basics/Expressions.scala
@@ -0,0 +1,8 @@
+package lectures.part1basics
+
+object Expressions extends App {
+  val x = 1 + 2 // EXPRESSION
+  println(x)
+  println(2 + 3 * 4)
+  // + - * / & | ^ << >> >>> (right shift with zero extension)
+}
